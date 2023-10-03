b580c70a46895c642d60bb1c09463ab50bcecff4
Никита <petrov.nikita702@mail.ru>
2023-10-03 10:25:54 +0300

    Проект

---
+++ js/header/header.js
@@ -0,0 +1,8 @@
+function burgerMenuShow(e) {
+
+}    
+const menu = document.querySelector(".burger-menu__button");
+menu.addEventListener("click", () =>{
+    const headerBlock = document.querySelector(".header");
+    headerBlock.classList.toggle("burger-menu__button_active")
+})
