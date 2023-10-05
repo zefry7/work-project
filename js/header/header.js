e03dbafab4f5b419822e40726d61bcdef9cf7d7a
Никита <petrov.nikita702@mail.ru>
2023-10-05 19:32:40 +0300

    Перенёс половину данных в index и сделал анимацию у пунктов в header

--- js/header/header.js
+++ js/header/header.js
@@ -1,6 +1,80 @@
-
+const headerBlock = document.querySelector(".header");
 const menu = document.querySelector(".burger-menu__button");
-menu.addEventListener("click", () =>{
-    const headerBlock = document.querySelector(".header");
+menu.addEventListener("click", () => {
     headerBlock.classList.toggle("burger-menu__button_active")
+    document.body.classList.toggle("body-blocker")
+})
+
+window.addEventListener("resize", () => {
+    if (screen.width > 768 && headerBlock.className.indexOf("burger-menu__button_active") != -1) {
+        headerBlock.classList.remove("burger-menu__button_active")
+    }
+})
+
+const links = document.querySelectorAll(".header__link");
+
+links.forEach(link => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault()
+
+        if (link.className.indexOf("burger-menu__link") != -1) {
+            headerBlock.classList.remove("burger-menu__button_active")
+            document.body.classList.remove("body-blocker")
+        }
+
+        const pos = document.getElementsByClassName(link.getAttribute("data-scroll"))[0].getBoundingClientRect().top + window.scrollY - 80;
+
+        window.scrollTo({
+            top: pos,
+            behavior: 'smooth'
+        });
+
+    })
+})
+
+const logo = document.querySelector(".header__block-logo");
+
+logo.addEventListener("click", (event) => {
+    event.preventDefault()
+
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+})
+
+
+const addAnim = (value) => {
+    value.forEach(el => {
+        if (el.target.className.indexOf("anim-block_active") == -1)
+            el.target.classList.toggle("anim-block_active", el.isIntersecting)
+    })
+}
+const options = {
+    rootMagin: "0px 0px 0px 200px",
+    threshold: 0
+}
+
+const observer = new IntersectionObserver(addAnim, options)
+
+const list = document.querySelectorAll(".anim-block")
+list.forEach(value => {
+    observer.observe(value)
+})
+
+
+const listScroll = document.querySelectorAll(".scroll")
+
+listScroll.forEach((value, index) => {
+    document.addEventListener("scroll", () => {
+        const posBlock = document.getElementsByClassName(value.classList[0])[0].getBoundingClientRect().top
+        const heightBlock = document.getElementsByClassName(value.classList[0])[0].getBoundingClientRect().height
+
+        if (posBlock - window.innerHeight / 2 < 0 && posBlock - window.innerHeight / 2 > (-1) * heightBlock) {
+            links[index].classList.add("header__link_use");
+        }
+        else {
+            links[index].classList.remove("header__link_use");
+        }
+    })
 })
